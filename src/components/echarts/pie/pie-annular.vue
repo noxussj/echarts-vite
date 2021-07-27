63406d23732ee41b96ec193fa74ef1345e21f69a
noxussj <632922356@qq.com>
2021-07-27 19:29:04 +0800

    添加插拔式开关

--- src/components/echarts/pie/pie-annular.vue
+++ src/components/echarts/pie/pie-annular.vue
@@ -17,6 +17,20 @@ export default {
             dom: this.$refs.echart,
             param: {
                 data: data,
+
+                // 插拔式开关（图例格式化）
+                legend: {
+                    isPercent: true, // 是否展示百分比
+                    col: 3, // 展示几列
+                    bottom: 20, // 距离底部
+                    left: 50, // 距离左侧
+                    space: 130, // 列间隔
+                },
+                radius: [40, 60], // 半径大小
+                center: ['center', 120], // 位置
+                isTransparent: true, // 插拔式开关（半透明高亮）
+                isTotal: true, // 插拔式开关（是否展示总数）
+                isSplitLine: true, // 插拔式开关（是否使用分割线）
             },
             opt: {},
         });
