649cc82a2f2a6db84f4b7ab9e8e851bf524eead5
noxussj <632922356@qq.com>
2021-07-29 16:34:25 +0800

    饼图添加扩展功能

--- src/components/echarts/pie/pie-annular.vue
+++ src/components/echarts/pie/pie-annular.vue
@@ -20,10 +20,18 @@ export default {
 
                 radius: [40, 60], // 半径大小
                 center: ['center', 120], // 位置
-                isLegendPercent: true, // 插槽式开关（图例百分比）
+
+                // 插拔式开关（渐变色）
+                gradient: {
+                    start: ['rgba(168, 39, 255, 1)', 'rgba(130, 39, 255, 1)', 'rgba(90, 0, 255, 1)', 'rgba(31, 84, 255, 1)', 'rgba(46, 41, 255, 1)'],
+                    end: ['rgba(6, 0, 255, 1)', 'rgba(11, 80, 255, 1)', 'rgba(0, 168, 255, 1)', 'rgba(0, 204, 255, 1)', 'rgba(15, 241, 255, 1)'],
+                },
+                // color: ['rgba(0, 255, 612, 1)', 'rgba(6, 0, 255, 1)'], // 插拔式开关（颜色，和渐变色冲突，只能开启其中一个）
+                isLegendPercent: true, // 插拔式开关（图例百分比）
                 isTransparent: true, // 插拔式开关（半透明高亮）
                 isTotal: true, // 插拔式开关（是否展示总数）
                 isSplitLine: true, // 插拔式开关（是否使用分割线）
+                splitColor: 'rgba(255, 255, 255, 1)', // 插拔式开关（分割线颜色）
             },
             opt: {},
         });
