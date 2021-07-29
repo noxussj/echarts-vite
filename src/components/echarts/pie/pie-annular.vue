57675a65c5e990f0758fb303afcab9fa2b76dfda
noxussj <632922356@qq.com>
2021-07-29 14:50:17 +0800

    暂时性修复饼图bug

--- src/components/echarts/pie/pie-annular.vue
+++ src/components/echarts/pie/pie-annular.vue
@@ -5,7 +5,7 @@
 <script>
 export default {
     mounted() {
-        let data = [
+        const data = [
             { value: 1048, name: '搜索引擎' },
             { value: 735, name: '直接访问' },
             { value: 580, name: '邮件营销' },
@@ -18,16 +18,9 @@ export default {
             param: {
                 data: data,
 
-                // 插拔式开关（图例格式化）
-                legend: {
-                    isPercent: true, // 是否展示百分比
-                    col: 3, // 展示几列
-                    bottom: 20, // 距离底部
-                    left: 50, // 距离左侧
-                    space: 130, // 列间隔
-                },
                 radius: [40, 60], // 半径大小
                 center: ['center', 120], // 位置
+                isLegendPercent: true, // 插槽式开关（图例百分比）
                 isTransparent: true, // 插拔式开关（半透明高亮）
                 isTotal: true, // 插拔式开关（是否展示总数）
                 isSplitLine: true, // 插拔式开关（是否使用分割线）
